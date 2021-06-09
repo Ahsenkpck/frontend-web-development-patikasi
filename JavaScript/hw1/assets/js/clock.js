3cb85be9a9ec1b4f0ced66a4146211cb8f25c079
Ahsen Kıpçak <akipcak01@gmail.com>
2021-06-09 04:58:20 +0300

    Update clock.js

--- JavaScript/hw1/assets/js/clock.js
+++ JavaScript/hw1/assets/js/clock.js
@@ -1,23 +1,23 @@
-let hrElement = document.querySelector(".hr")
-let minElement = document.querySelector(".min")
+  let hrElement = document.querySelector(".hr");
+let minElement = document.querySelector(".min");
 let secElement = document.querySelector(".sec");
-let nameElement = document.querySelector("#myName")
+let nameElement = document.querySelector("#myName");
 let dayElement = document.querySelector(".day");
-const days = ["Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi", "Pazar"]
+const days = ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"];
 
 let hr, min, sec, day, name;
-name = window.prompt("Adınız nedir? ")
+name = window.prompt("Adınız nedir? ");
 nameElement.innerHTML = name;
 
-setInterval(function () {
+setInterval(function(){
     hr = new Date().getHours();
     min = new Date().getMinutes();
     sec = new Date().getSeconds();
-    day = new Date().getUTCDay();
+    day = new Date().getDay();
 
-    hrElement.innerHTML = hr < 10 ? `0${hr}` : hr;
-    minElement.innerHTML = min < 10 ? `0${min}` : min;
-    secElement.innerHTML = sec < 10 ? `0${sec}` : sec;
-    dayElement.innerHTML = days[day-1];
+    hrElement.innerHTML = hr<10 ? `0${hr}`: hr;
+    minElement.innerHTML = min<10 ? `0${min}` : min;
+    secElement.innerHTML = sec<10 ? `0${sec}` : sec;
+    dayElement.innerHTML = days[day];
 
-},1000)
+},1000);
