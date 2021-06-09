994c585ee4f8d3def51ae49997a4044fa030daa1
Ahsen Kıpçak <akipcak01@gmail.com>
2021-06-09 04:52:35 +0300

    hw1 completed.

---
+++ JavaScript/hw1/assets/js/clock.js
@@ -0,0 +1,23 @@
+let hrElement = document.querySelector(".hr")
+let minElement = document.querySelector(".min")
+let secElement = document.querySelector(".sec");
+let nameElement = document.querySelector("#myName")
+let dayElement = document.querySelector(".day");
+const days = ["Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi", "Pazar"]
+
+let hr, min, sec, day, name;
+name = window.prompt("Adınız nedir? ")
+nameElement.innerHTML = name;
+
+setInterval(function () {
+    hr = new Date().getHours();
+    min = new Date().getMinutes();
+    sec = new Date().getSeconds();
+    day = new Date().getUTCDay();
+
+    hrElement.innerHTML = hr < 10 ? `0${hr}` : hr;
+    minElement.innerHTML = min < 10 ? `0${min}` : min;
+    secElement.innerHTML = sec < 10 ? `0${sec}` : sec;
+    dayElement.innerHTML = days[day-1];
+
+},1000)
